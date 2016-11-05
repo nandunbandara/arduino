9208a5944ca88872ac561ec895e6fa3680335b56
Nandun Bandara <ntbandara3@hotmail.com>
2016-11-05 13:55:48 +0530

    inf read

---
+++ inf_read.ino
@@ -0,0 +1,17 @@
+
+int inf = A0;
+int value = 0;
+void setup() {                
+  pinMode(inf, INPUT);
+  Serial.begin(9600);  
+}
+
+void loop() {
+   value=analogRead(inf);
+   if(value!=1023 && value!=1 && value != 0){
+     Serial.println(value);
+     for(int i=0;i<value;i+=10){
+       Serial.print("#");
+     }
+   }
+}
